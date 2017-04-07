e165d2ca8edc89fe6bf4f51cb62703c7777ae4f2
oglab <n.nurjamal@gmail.com>
2017-04-07 15:45:03 +0700

    1st

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.layouts.default')
+
+@section('page-level-styles')
+
+@endsection
+
+@section('content')
+    <section class="k-main">
+        Main
+    </section>
+@endsection
+
+@section('page-level-scripts')
+
+@endsection
